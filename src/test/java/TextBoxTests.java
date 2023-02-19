08b895f61931bd3d502a0376134457e8e45494e6
Aleksey Dunaev <gvards.a@gmail.com>
2023-02-19 15:24:52 +0400

    added TextBoxTests

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beroreAll() {
+        Configuration.browserSize = "2560x1440";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+//        $("[id=userName]").setValue("Aleksey Dunaev");
+        $("#userName").setValue("Aleksey Dunaev");
+        $("#userEmail").setValue("aleks@mail.com");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Aleksey Dunaev");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Aleksey Dunaev"), text("aleks@mail.com"),
+                text("Some address 1"), text("Aleksey Dunaev"));
+
+
+    }
+}
